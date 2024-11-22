82914e06764d6821dbf0ce782832417f62f12de2
Kayxue <s0990006@gmail.com>
2024-11-22 23:56:59 +0800

    Init

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::sync::{Mutex,Arc};
+use std::thread;
+use std::env;
+use std::thread::JoinHandle;
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    let x: i128 = args[1].parse().unwrap();
+    let n: i128 = args[2].parse().unwrap();
+    let mut handler: Vec<JoinHandle<()>> = Vec::new();
+
+    let sum = Arc::new(Mutex::new(0i128));
+    for i in 1..=x{
+        let locker= Arc::clone(&sum);
+        handler.push(thread::spawn(move || {
+            println!("Thread {} started", i);
+            let startFrom = (i - 1) * (n / x) + 1;
+            let end = if i == x { n } else { i * (n / x) };
+            for i in startFrom..=end {
+                {
+                    let mut sum = locker.lock().unwrap();
+                    *sum += i;
+                }
+            }
+        }));
+    }
+
+    for handle in handler{
+        handle.join().unwrap();
+    }
+
+    println!("Sum: {:?}", *sum.lock().unwrap());
+}
